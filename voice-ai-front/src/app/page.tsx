43df12470ec1a560093c56f2fdd1337f7a232b7b
Jake <ljh95714@gmail.com>
2023-12-21 15:08:03 +0700

    audio record button addition

--- voice-ai-front/src/app/page.tsx
+++ voice-ai-front/src/app/page.tsx
@@ -1,31 +1,76 @@
+'use client'
 import * as React from 'react'
 import Box from '@mui/material/Box'
-import Grid from '@mui/material/Unstable_Grid2'
-import Drawer from '@mui/material/Drawer'
-import Typography from '@mui/material/Typography'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import Alert from '@mui/material/Alert'
-import AlertTitle from '@mui/material/AlertTitle'
-import MediaCard from '@/components/MediaCard'
 import { Button } from '@mui/material'
+import { useState } from 'react'
 
 export default function HomePage() {
+    const [recording, setRecording] = useState(false)
+    const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+        null
+    )
+
+    const handleClick = async () => {
+        if (!recording) {
+            if (
+                !navigator.mediaDevices ||
+                !navigator.mediaDevices.getUserMedia
+            ) {
+                console.log('getUserMedia not supported on your browser!')
+                return
+            }
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+            })
+            const newMediaRecorder = new MediaRecorder(stream)
+            let chunks: BlobPart[] = []
+
+            newMediaRecorder.ondataavailable = function (e) {
+                chunks.push(e.data)
+            }
+
+            newMediaRecorder.onstop = function () {
+                const blob = new Blob(chunks, {
+                    type: 'audio/ogg; codecs=opus',
+                })
+                console.log('Blob properties:', {
+                    size: blob.size,
+                    type: blob.type,
+                })
+                chunks = []
+                const audioURL = URL.createObjectURL(blob)
+                const audio = new Audio(audioURL)
+                audio.play()
+            }
+            newMediaRecorder.start()
+            setMediaRecorder(newMediaRecorder)
+        } else {
+            if (mediaRecorder) {
+                mediaRecorder.stop()
+            }
+        }
+        setRecording(!recording)
+    }
     return (
         <Box
             sx={{
                 display: 'flex',
-                flexDirection: { xs: 'column', md: 'row' },
                 alignItems: 'center',
                 justifyContent: 'center',
                 bgcolor: 'background.paper',
-                overflow: 'hidden',
                 borderRadius: '12px',
-                boxShadow: 1,
-                fontWeight: 'bold',
+                boxShadow: 2,
+                minHeight: '40vh',
             }}
         >
-            <Button variant="contained">Record</Button>
+            <Button
+                variant="contained"
+                sx={{ borderRadius: '14px', height: '40px' }}
+                color={recording ? 'error' : 'primary'}
+                onClick={handleClick}
+            >
+                Record
+            </Button>
         </Box>
     )
 }
